270b9a7ecc9cf5095dafa5d71ab43e9b8614b562
walterfjustino <walterfjustino@gmail.com>
2019-09-22 22:35:30 -0300

    updatenessaporra

---
+++ validar.js
@@ -0,0 +1,28 @@
+
+/*
+function validar() {
+    if (arquivo.length != colspan 241) {
+
+        return alert("arquivo maior que o Layout Multipag!");
+        
+    } else {
+
+        return validar();
+        
+    }
+    
+}
+
+
+<script> 
+            
+            var arquivo = window.prompt('insira o arquivo...')
+            document.write(`Arquivo tem ${arquivo.length} posições.`)
+            if (condition) {
+                
+            } else {
+                
+            }
+            
+            
+            </script>*/
